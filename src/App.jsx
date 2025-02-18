194e8613bc8f047ef2d5132f87c1cf975eb525a8
hub-mol <113196877+hub-mol@users.noreply.github.com>
2025-02-18 20:33:38 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { useState } from "react";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const [shoppingCart, setShoppingCart] = useState({ itmes: [] });
+
+    return (
+        <div>
+            <Header
+                // { items: [] }
+                cart={shoppingCart}
+            />
+            <Shop onAddItemToCart={() => setShoppingCart} />
+        </div>
+    );
 }
 
 export default App;
